f571498c3d746ad3a35f7e8a2df1d646b2cd32cf
nemesisvoid <samsonajibade40@gmail.com>
2025-07-03 21:04:22 +0100

    added filtering, pagination and minor fixes

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,8 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-06-27';
 
-export const dataset = assertValue(process.env.SANITY_STUDIO_DATASET, 'Missing environment variable: SANITY_STUDIO_DATASET');
+export const dataset = assertValue(process.env.SANITY__STUDIO_DATASET, 'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET');
 
-export const projectId = assertValue(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID');
+export const projectId = assertValue(process.env.SANITY_STUDIO_PROJECT_ID, 'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID');
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
