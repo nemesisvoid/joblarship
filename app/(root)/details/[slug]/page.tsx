b423f090f23a989ea12b2feda3fe4071c46e53ed
nemesisvoid <samsonajibade40@gmail.com>
2025-07-12 09:21:44 +0100

    minor changes

--- app/(root)/details/[slug]/page.tsx
+++ app/(root)/details/[slug]/page.tsx
@@ -4,6 +4,34 @@ import { Opportunities } from '@/sanity/types';
 import { getOpportunityBySlug } from '@/sanity/lib/queries';
 import Details from './details';
 
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const data: Opportunities = await client.fetch(getOpportunityBySlug, { slug: params.slug });
+
+  return {
+    title: data.title,
+    description: data.shortdesc,
+    openGraph: {
+      title: data.title,
+      description: data.description,
+      url: `https://joblarship.com/details/${params.slug}`,
+      type: 'website',
+      images: [
+        {
+          url: data.image, // Make sure it's a full image URL
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: data.title,
+      description: data.description,
+      images: [data.image],
+    },
+  };
+}
+
 const DetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
   const { slug } = await props.params;
   const data: Opportunities = await client.fetch(getOpportunityBySlug, { slug });
